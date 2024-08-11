32af7f6e43d655c96ce592d388a1469c789389fb
Thomas Farias <thomas.farias@sempreit.com.br>
2024-08-11 19:26:03 -0300

    Componente Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
+    <html lang="pt" className={inter.variable}>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
