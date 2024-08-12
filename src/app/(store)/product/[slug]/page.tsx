4156bf026ab419ea0fe7a47e8f09849909bd8077
Thomas Farias <thomas.farias@sempreit.com.br>
2024-08-11 21:10:08 -0300

    Page metadata

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { api } from '@/data/api'
 import type { Product } from '@/data/types/product'
+import type { Metadata } from 'next'
 import Image from 'next/image'
 
 interface ProductProps {
@@ -16,6 +17,15 @@ async function getProduct(slug: string): Promise<Product> {
   const products = await response.json()
 
   return products
+}
+
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug)
+  return {
+    title: product.title,
+  }
 }
 
 export default async function ProductPage({ params }: ProductProps) {
