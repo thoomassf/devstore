2268ba669aa24f3143716bfc322a8e5d3379b72d
Thomas Farias <thomas.farias@sempreit.com.br>
2024-08-11 21:57:39 -0300

    Add products to cart

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from '@/components/add-to-cart-button'
 import { api } from '@/data/api'
 import type { Product } from '@/data/types/product'
 import type { Metadata } from 'next'
@@ -108,12 +109,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
---
+++ src/components/add-to-cart-button.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useCart } from '@/contexts/cart-context'
+
+interface AddtoCartButtonProps {
+  productId: number
+}
+
+export function AddToCartButton({ productId }: AddtoCartButtonProps) {
+  const { addToCart } = useCart()
+
+  function handleAddProductToCart() {
+    addToCart(productId)
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleAddProductToCart}
+      className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
+    >
+      Adicionar ao carrinho
+    </button>
+  )
+}
